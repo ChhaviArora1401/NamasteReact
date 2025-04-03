909e110b7b55c5c3f712bdc84cb3a8b0aca29c14
Chhavi Arora <chhaviarora1401@gmail.com>
2025-04-04 01:25:30 +0530

    Ep3

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import ReactDOM from "react-dom/client";
 
 // JSX (transpiled before it reaches the JS Engine) - PARCEL - Babel
 
-// JSX =>Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
 
 // React Element
 // const heading = (
@@ -18,9 +18,25 @@ import ReactDOM from "react-dom/client";
 //     Namaste React using JSX
 //   </h1>);
 
+// React Component (Arrow function)
 const Title = () => (
   <h1 id="heading1" className="head" tabIndex="1">
     Namaste React using JSX
+  </h1>);
+
+// React component (function)
+// const Title = function () {
+//   return (
+//   <h1 id="heading1" className="head" tabIndex="1">
+//     Namaste React using JSX
+//   </h1>);
+//   };
+
+const elem = <span>Hey</span>
+// React Element
+const title = (
+  <h1 id="heading1" className="head" tabIndex="1">
+   {elem} Namaste React using JSX
   </h1>);
 
 // React Component
@@ -30,20 +46,27 @@ const Title = () => (
 const fn = () => true;
 const fn1 = () => { return true };
 
-// React Functional Component --  function that returns some JSX
-// Component Composition
+const number = 10000;
+
+// React Functional Component --  function that returns some JSX / React Element
+// Component Composition (jargon)
 const HeadingComponent = () => {
   return <div id="container">
-    <Title />
+    <h2>{number}</h2> {100 + 200} {console.log("a")} {/* any javaScript expression */}
+    <Title /> {/*component*/}
+    <Title></Title>
+    {Title()}
+    {title} {/*element*/}
     <h1 className="heading1">
       React Functional Component
     </h1>
   </div>;
 }
 
+// without return statement
 const HeadingComponent1 = () => (<h1 className="heading1">React Functional Component</h1>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(<HeadingComponent />);
+// root.render(heading); // for React Element Rendering
+root.render(<HeadingComponent />); // for React Component Rendering
